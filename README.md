bd28f398ef0002b5ae23fc67171c5518a2eeeab8
Zack Harley <zackharley@gmail.com>
2022-02-24 15:34:17 -0500

    Properly handle cities with multiple words

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 > Unofficial [Numbeo Cost of Living](https://www.numbeo.com/cost-of-living/) JSON API
 
+## Getting Started
+
+First, install dependencies:
+
+```sh
+npm install
+```
+
+Then run the application in development mode:
+
+```sh
+npm run dev
+```
+
 ## API
 
 ### `/:city?currency`
@@ -19,4 +33,3 @@ The city for which you want to retrieve cost-of-living data for
 ##### `currency`
 
 The three letter currency code for the currency you want values in. Defaults to CAD.
-
--- index.js
+++ index.js
@@ -1,62 +1,73 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const _ = require('lodash');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
 });
 
 app.get('/:city', async (req, res) => {
-    const city = req.params.city[0].toUpperCase() + req.params.city.slice(1).toLowerCase();
-    const { currency = 'CAD' } = req.query;
+  const city = _.words(_.startCase(req.params.city)).join('-');
+  const { currency = 'CAD' } = req.query;
 
-    const response = await fetch(`https://www.numbeo.com/cost-of-living/in/${city}?displayCurrency=${currency}`);
-    if (!response.ok) {
-        return res.status(response.status).send(response.statusText);
-    }
-    const html = await response.text();
+  const response = await fetch(
+    `https://www.numbeo.com/cost-of-living/in/${city}?displayCurrency=${currency}`,
+  );
+  if (!response.ok) {
+    return res.status(response.status).send(response.statusText);
+  }
+  const html = await response.text();
 
-    const $ = cheerio.load(html);
+  const $ = cheerio.load(html);
 
-    const rows = $('body > div.innerWidth > table > tbody > tr')
-        .filter((i, el) => $(el).children('td').length === 3)
-        .map(
-            (i, el) => $(el).children().map(
-                (i, el) => $(el).text().trim()
-            ).toArray()
-        ).toArray();
+  const rows = $('body > div.innerWidth > table > tbody > tr')
+    .filter((i, el) => $(el).children('td').length === 3)
+    .map((i, el) =>
+      $(el)
+        .children()
+        .map((i, el) => $(el).text().trim())
+        .toArray(),
+    )
+    .toArray();
 
-    const costs = chunkArray(rows, 3)
-        .map(([item, costWithSymbol, range]) => {
-            const cost = costWithSymbol.replace(/^.*?([\d,.]+).*?$/, '$1');
-            const [rangeLow, rangeHigh] = range.split('-');
-            return {
-                item,
-                cost,
-                range: {
-                    low: rangeLow,
-                    high: rangeHigh
-                }
-            }
-        });
-    return res.json({ city, costs, currency });
+  const costs = chunkArray(rows, 3).map(([item, costWithSymbol, range]) => {
+    const cost = costWithSymbol.replace(/^.*?([\d,.]+).*?$/, '$1');
+    const [rangeLow, rangeHigh] = range.split('-');
+    return {
+      item,
+      cost,
+      range: {
+        low: rangeLow,
+        high: rangeHigh,
+      },
+    };
+  });
+  return res.json({ city, currency, costs });
 });
 
-app.get('*', (req, res) => res.status(400).json({
-    error: 'No city supplied. Please navigate to `/:city` to obtain results.'
-}));
+app.get('*', (req, res) =>
+  res.status(400).json({
+    error: 'No city supplied. Please navigate to `/:city` to obtain results.',
+  }),
+);
 
 function chunkArray(arr, chunkSize) {
-    let temp = [];
-    for (let i = 0; i < arr.length; i += chunkSize) {
-        temp.push(arr.slice(i, i + chunkSize));
-    }
-    return temp;
+  let temp = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    temp.push(arr.slice(i, i + chunkSize));
+  }
+  return temp;
 }
 
-app.listen(PORT, () => console.log(`Cost of Living API running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Cost of Living API running on port ${PORT}`),
+);
